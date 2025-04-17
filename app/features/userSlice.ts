24655f3ad31b6615754dfb1890a7b83638da3098
stodo <stodoo@gmail.com>
2025-04-17 16:16:27 +0200

    userSlice.js -> typed version .ts

--- app/features/userSlice.ts
+++ app/features/userSlice.ts
@@ -1,6 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = {
+interface UserState {
+  firstName: string;
+  email: string;
+  pictureUrl: string;
+}
+
+interface UserPayload {
+  firstName?: string;
+  email?: string;
+  pictureUrl?: string;
+}
+
+const initialState: UserState = {
   firstName: "",
   email: "",
   pictureUrl: "",
@@ -10,7 +22,7 @@ export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setUser: (state, action) => {
+    setUser: (state, action: PayloadAction<UserPayload>) => {
       state.firstName = action.payload.firstName || "";
       state.email = action.payload.email || "";
       state.pictureUrl = action.payload.pictureUrl || "";
