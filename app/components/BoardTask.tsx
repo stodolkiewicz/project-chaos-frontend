5d2df5a7896e144c38831d6e4b389b2085011210
stodo <stodoo@gmail.com>
2025-04-27 02:40:13 +0200

    styling of BoardTasks

--- app/components/BoardTask.tsx
+++ app/components/BoardTask.tsx
@@ -6,15 +6,20 @@ interface BoardTaskProps {
 
 export default function BoardTask({ boardTask }: BoardTaskProps) {
   return (
-    <div className="rounded-2xl p-2 m-2 flex flex-col border-2">
+    <div className="rounded-2xl p-2 m-2 flex flex-col border-2 shadow-sm hover:shadow-md transition-shadow duration-300 cursor-pointer">
       <div className="flex">
-        <h6 className="flex-1/2">{boardTask.title}</h6>
-        <div
-          className="w-3 h-2 rounded-full"
-          style={{ backgroundColor: boardTask.priority.color }}
-        ></div>
+        <h6 className="flex-1">{boardTask.title}</h6>
+        {/* priority colorful square section */}
+        <div className="flex flex-col items-end">
+          <div
+            className="w-2 h-1 rounded-full"
+            style={{ backgroundColor: boardTask.priority.color }}
+          ></div>
+          <div className=" text-xs text-muted-foreground font-medium">
+            {boardTask.priority.name}
+          </div>
+        </div>
       </div>
-      {boardTask.priority.color}
       <div>{boardTask.description}</div>
     </div>
   );
