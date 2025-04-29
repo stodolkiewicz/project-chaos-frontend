476a1f1a0de94d9f29b0de3c5aed65e390fed929
stodo <stodoo@gmail.com>
2025-04-29 19:11:50 +0200

    Display of BoardTaskFooter - responsive

--- app/dashboard/components/BoardTask/BoardTaskFooter.tsx
+++ app/dashboard/components/BoardTask/BoardTaskFooter.tsx
@@ -6,8 +6,8 @@ export default function BoardTaskFooter({
   assigneeEmail,
 }: BoardTaskFooterProps) {
   return (
-    <div className="flex mt-3">
-      <div className="text-xs font-medium flex-1">assigned to:</div>
+    <div className="flex flex-col xl:flex-row mt-3">
+      <div className="text-xs font-medium flex-1">assigned&nbsp;to:</div>
       <div className="text-xs">{assigneeEmail}</div>
     </div>
   );
