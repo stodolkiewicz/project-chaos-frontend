326ddba7f50bb3b86f0c1871ffa83503ef30bfa5
stodo <stodoo@gmail.com>
2025-04-29 04:11:20 +0200

    Add Task icon went to bottom of a column. Header - color change

--- app/components/TopMenu/TopMenu.tsx
+++ app/components/TopMenu/TopMenu.tsx
@@ -5,7 +5,7 @@ export default function TopMenu() {
   const userData = useAppSelector((state) => state.user);
 
   return (
-    <div className="fixed top-0 left-0 w-screen h-[48px] flex items-center px-4 border-b-1">
+    <div className="bg-primary opacity-96 fixed top-0 left-0 w-screen h-[2.8rem] flex items-center px-4 border-b-1">
       <div className="flex-1" />
       <div className="flex justify-end items-center gap-2 mr-10">
         {userData ? (
--- app/dashboard/components/Column.tsx
+++ app/dashboard/components/Column.tsx
@@ -9,17 +9,20 @@ export default function Column({
   return (
     <div
       key={column.id}
-      className="box-border border-2 rounded-sm ml-2 mr-2 min-h-[30rem]"
+      className="flex flex-col box-border border-2 rounded-sm ml-2 mr-2 mb-20 min-h-[30rem]"
       style={{ width: `${columnWidthPercentage}%` }}
     >
-      <h5 className="group flex items-center px-4 py-2 bg-slate-100 hover:bg-slate-200 bg-primary-headers-darker hover:bg-primary-headers-darker transition-all duration-300">
+      <h5 className="flex items-center px-4 py-2 bg-slate-100 hover:bg-slate-200 bg-primary-headers-darker hover:bg-primary-headers-darker transition-all duration-300">
         <span className="flex flex-1 justify-center">{column.name}</span>
-        <Plus className=" hover:scale-115 text-green-600 w-7 h-7 p-1 hover:border-slate-300 hover:border rounded-full transition-all duration-300 cursor-pointer" />
       </h5>
 
       {tasksInColumn.map((taskInColumn) => (
         <BoardTask key={taskInColumn.taskId} boardTask={taskInColumn} />
       ))}
+
+      <div className="w-full flex flex-1 justify-end items-end px-2 py-3">
+        <Plus className="w-7 h-7 p-1 text-primary hover:scale-110 hover:border-slate-200 hover:border rounded-full transition-all duration-300 cursor-pointer" />
+      </div>
     </div>
   );
 }
